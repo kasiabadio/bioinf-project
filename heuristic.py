192fa7868620e169a62c9a50997b055a2be951f5
Morsdors <dorota.solarska@student.put.poznan.pl>
2022-06-13 21:23:40 +0200

    fine tuning the number of iterations

--- heuristic.py
+++ heuristic.py
@@ -136,28 +136,36 @@ def main_tabu():
     
     visited_with_counter_copy = visited_with_counter.copy()
     # add olis to lmers from first greedy solution
-    greedy_solution = greedy_algorithm()
+    for j in range(4):
+        greedy_solution = greedy_algorithm()
+        reference_set.append(greedy_solution)
+
     visited_with_counter = visited_with_counter_copy
+
     for i in range(len(greedy_solution)-1):
         to_add = greedy_solution[i:(i+K)]
         if to_add in lmers:
             lmers[to_add] += 1
-
+    
+    reference_set = sorted(reference_set, key=lambda x: len(x), reverse=True)
     best_solution = greedy_solution
-    reference_set = [best_solution, '']
+    #reference_set = [best_solution, '']
  
     # 3) while not all restarts done
     loop_count = 0
+    loop_count_outer = 0
 
     lenghts_tab = []
     del_insert_tab = [[],[]]
-    while loop_count < 20:  
-        print("loop_count: ", loop_count)
+    temporary_best = ["", 0]
+    while loop_count_outer < 10:
+            
+        greedy_solution = greedy_algorithm()
+        reference_set.append(greedy_solution)
         # replace the worst solution from reference_set by greedy solution
         if len(reference_set) > 1:
             greedy_solution = greedy_algorithm()
             visited_with_counter = visited_with_counter_copy
-
             reference_set = restart(reference_set, greedy_solution)
 
         reference_set = sorted(reference_set, key=lambda x: len(x), reverse=True)
@@ -165,88 +173,106 @@ def main_tabu():
 
         first_sol = best_solution
         print("\nbest_solution before extending/deleting: \n", best_solution)
-        
-        
+        print("\nREFERENCE SET SIZE:", len(reference_set))
 
-        #print("reference_set after sorting ", reference_set)
+        while loop_count < 8:  
+           
+            #print("reference_set after sorting ", reference_set)
 
-        # 4) while not all cycles of restarts do 12, 13
-        deletion_count = 0
-        insertion_count = 0
+            # 4) while not all cycles of restarts do 12, 13
+            deletion_count = 0
+            insertion_count = 0
 
-
-        for i in range(400):
-            #save full length solutions
-            if(len(best_solution) == N and best_solution not in final_solutions):
-                final_solutions.append(best_solution)
-                reference_set.pop(0)
-                greedy_solution = greedy_algorithm()
-                reference_set.append( greedy_solution )
-                visited_with_counter = visited_with_counter_copy
+            for i in range(400):
+                #save full length solutions
+                if(len(best_solution) == N and best_solution not in final_solutions):
+                    final_solutions.append(best_solution)
+                    reference_set.pop(0)
+                    greedy_solution = greedy_algorithm()
+                    reference_set.append( greedy_solution )
+                    visited_with_counter = visited_with_counter_copy
 
             
-            #print("TABU len ", len(tabu))
-            #print("VISITED WITH COUNTER: ", visited_with_counter)
-            # 12, 13) insertion or deletion of an lmer
-            if(len(olis)>=10):
-                if len(tabu) >= len(olis)//10:      #tabu_size
-                    tabu.pop(0)
-            else:
-                if len(tabu) >= 3:      #tabu_size
-                    tabu.pop(0)
+                #print("TABU len ", len(tabu))
+                #print("VISITED WITH COUNTER: ", visited_with_counter)
+                # 12, 13) insertion or deletion of an lmer
+                if(len(olis)>=10):
+                    if len(tabu) >= len(olis)//10:      #tabu_size
+                        tabu.pop(0)
+                else:
+                    if len(tabu) >= 3:      #tabu_size
+                        tabu.pop(0)
 
-            is_inserted = False
+                is_inserted = False
+                #sortujemy olis wg częstotliwości w rozwiązaniach
+
+
+                #CO TU SIe BeDZIE DZIAlO :OOOOO
+
+                olis = sorted(lmers, key=lambda lmer: lmers[lmer])
+                #print("\nbest_solution before extending/deleting: ", best_solution)
+                for oli in olis:
+                    if oli not in tabu:
+                        # check counter in visited_with_counter && add fragment to lmers because it is useful
+                        if visited_with_counter[oli] > 0:
+                            is_inserted = extend_move_insert(oli)
+                            #print("I checked addition for ", oli, " and it is ", is_inserted)
+                            if is_inserted:
+                                lmers[oli] += 1
+                                visited_with_counter[oli] -= 1
+                                #print("counter + ", oli)
+                                insertion_count+=1
+                                #print("best_solution after adding: ", best_solution)
+                                break
+
+                if not is_inserted:
+                    fragment = best_solution[len(best_solution)-K:]
+                    if fragment in lmers: #and fragment not in tabu:
+                        if lmers[fragment] > 0:
+                            # increase counter in visited_with_counter
+                            if fragment in visited_with_counter:
+                                visited_with_counter[fragment] += 1
+                            else:
+                                visited_with_counter[fragment] = 1
+
+                            # delete fragment from lmers because it is not useful
+                            lmers[fragment] -= 1
+                            #print("counter - ", fragment)
+                            deletion_count+=1
+                            best_solution = best_solution[:-1]
+                            if fragment != S0:
+                                tabu.append(fragment)
+                    #else:
+                        #tabu.pop(-1)
+                        #tabu.pop(-1)
+                        #print("tabu", tabu)
+                        #final_solutions.append(best_solution)
+                        #break
+                            #print("best_solution after delete: ", best_solution)
+
+            #print("LMERS: ", lmers)
+            reference_set[0] = best_solution
+            #print("reference_set[0] ", reference_set[0])
             olis = sorted(lmers, key=lambda lmer: lmers[lmer])
-            #print("\nbest_solution before extending/deleting: ", best_solution)
-            for oli in olis:
-                if oli not in tabu:
-                    # check counter in visited_with_counter && add fragment to lmers because it is useful
-                    if visited_with_counter[oli] > 0:
-                        is_inserted = extend_move_insert(oli)
-                        #print("I checked addition for ", oli, " and it is ", is_inserted)
-                        if is_inserted:
-                            lmers[oli] += 1
-                            visited_with_counter[oli] -= 1
-                            #print("counter + ", oli)
-                            insertion_count+=1
-                            #print("best_solution after adding: ", best_solution)
-                            break
-
-            if not is_inserted:
-                fragment = best_solution[len(best_solution)-K:]
-                if fragment in lmers and fragment not in tabu:
-                    if lmers[fragment] > 0:
-                        # increase counter in visited_with_counter
-                        if fragment in visited_with_counter:
-                            visited_with_counter[fragment] += 1
-                        else:
-                            visited_with_counter[fragment] = 1
-
-                        # delete fragment from lmers because it is not useful
-                        lmers[fragment] -= 1
-                        #print("counter - ", fragment)
-                        deletion_count+=1
-                        best_solution = best_solution[:-1]
-                        if fragment != S0:
-                            tabu.append(fragment)
-                        #print("best_solution after delete: ", best_solution)
-
-        #print("LMERS: ", lmers)
-        reference_set[0] = best_solution
-        #print("reference_set[0] ", reference_set[0])
-        olis = sorted(lmers, key=lambda lmer: lmers[lmer])
+            temp_length = len(best_solution)
         
-        loop_count += 1
-        print("ina koniec", loop_count)
-        print("\n\nbest_solution after changes: ", best_solution)
-        print("Wydluzylismy o:", len(best_solution) - len(first_sol))
-        lenghts_tab.append(len(best_solution))
-        print("\ndeleted: ", deletion_count, " inserted", insertion_count)
-        print("Szukana dlugosc lancucha: ", N, " dlugosc znalezionego lancucha: ", len(best_solution))
-        del_insert_tab[0].append(deletion_count)
-        del_insert_tab[1].append(insertion_count)
-    print("\n\n------FINAL: \n searching for length: ", N, "\nour lengths: \n", lenghts_tab, "\n deletion count: \n", del_insert_tab[0], "insertion count: \n", del_insert_tab[1])
-
+            loop_count += 1
+            #print("\n\nbest_solution after changes: ", best_solution)
+            #print("Wydluzylismy o:", temp_length - len(first_sol))
+            lenghts_tab.append(temp_length)
+            #print("\ndeleted: ", deletion_count, " inserted", insertion_count)
+            #print("Szukana dlugosc lancucha: ", N, " dlugosc znalezionego lancucha: ", temp_length)
+            del_insert_tab[0].append(deletion_count)
+            del_insert_tab[1].append(insertion_count)
+            #zapisz najlepsze rozwiązanie z iteracji while
+            if(temporary_best[1]<temp_length):
+                temporary_best = [best_solution, temp_length]
+        loop_count_outer+=1
+    print("\n\n------FINAL: \n searching for length: ", N, "\nour lengths: \n", lenghts_tab, "\n deletion count: \n", del_insert_tab[0], "\ninsertion count: \n", del_insert_tab[1])
+    print("Best solution len: ", temporary_best[1])
+    final_solutions.append(temporary_best[1])
+    print("final", final_solutions)
+    reference_set.pop(0)
 
 
        
